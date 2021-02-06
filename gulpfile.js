1ea45b2cb86decb2635d213a304bcd494e518556
Rosumere <mail@yablochkin.su>
2021-02-06 22:21:46 +0200

    update this project - add styles function

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,70 @@
-const gulp = require('gulp');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const del = require('del');
-const browserSync = require('browser-sync').create();
-const concat = require('gulp-concat');
-const cleanCSS = require('gulp-clean-css');
-const sourcemaps = require('gulp-sourcemaps');
-const gulpif = require('gulp-if');
+const { src, dest, parallel, series, watch } = require("gulp");
+const del = require("del");
+const autoprefixer = require("gulp-autoprefixer");
+const gcmq = require("gulp-group-css-media-queries");
+const cleanCSS = require("gulp-clean-css");
+const rename = require("gulp-rename");
+const browserSync = require("browser-sync").create();
+const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
+const gulpif = require("gulp-if");
+const notify = require("gulp-notify");
+const svgSprite = require("gulp-svg-sprite");
+const htmlmin = require("gulp-htmlmin");
 
-const isDev = (process.argv.indexOf('--dev') !== -1);
-const isProd = !isDev;
+let isProd = false;
 
+function clean() {
+  return del(["build/*"]);
+}
+exports.clean = clean;
 
-gulp.task('clean', function() {
-  return del('build/*')
-})
+function html() {
+  return src("./src/*.html")
+    .pipe(gulpif(isProd, htmlmin({ collapseWhitespace: true })))
+    .pipe(dest("./build"))
+    .pipe(browserSync.stream());
+}
+exports.html = html;
 
-gulp.task('css', function () {
-  return gulp.src ('./src/css/**/*.css')
-    .pipe(gulpif(isDev, sourcemaps.init()))
-    .pipe(postcss( [ autoprefixer({ cascade: false }) ] ))
-    .pipe(gulpif(isProd, cleanCSS({level: 1})))
-    .pipe(concat('all.css'))
-    .pipe(gulpif(isDev, sourcemaps.write()))
-    .pipe(gulp.dest('./build/css'))
-    .pipe(browserSync.stream())
-});
+function styles() {
+  return src("./src/assets/sass/style.scss")
+    .pipe(gulpif(!isProd, sourcemaps.init()))
+    .pipe(sass().on("error", notify.onError()))
+    .pipe(gulpif(isProd, gcmq()))
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(cleanCSS({ level: 1 }))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulpif(!isProd, sourcemaps.write(".")))
+    .pipe(dest("./build/assets/css/"))
+    .pipe(browserSync.stream());
+}
+exports.styles = styles;
 
-gulp.task('html', function() {
-  return gulp.src ('./src/**/*.html')
-    .pipe (gulp.dest ('./build'))
-    .pipe(browserSync.stream())
-})
+function svgSprites() {
+  return src("./src/assets/img/svg/sprites/**.svg")
+    .pipe(
+      svgSprite({
+        mode: {
+          symbol: {
+            sprite: "../sprite.svg",
+          },
+        },
+      })
+    )
+    .pipe(dest("./build/assets/img/svg/"));
+}
+exports.svgSprites = svgSprites;
 
-gulp.task('browser-sync', function() {
+const watching = () => {
   browserSync.init({
-      server: {
-          baseDir: "./build/"
-      }
+    server: {
+      baseDir: "./build",
+    },
   });
-  gulp.watch('./src/css/**/*.css', gulp.parallel('css'))
-	gulp.watch('./src/**/*.html', gulp.parallel('html'))
-});
+  watch("./src/*.html", html);
+  watch("./src/assets/sass/**/*.scss", styles);
+  watch("./src/assets/img/svg/sprites/**.svg", svgSprites);
+};
 
-gulp.task('build', gulp.series('clean', gulp.parallel('css', 'html')));
-gulp.task('watch', gulp.series('build', gulp.parallel('browser-sync'))); 
+exports.default = series(clean, styles, svgSprites, html, watching);
